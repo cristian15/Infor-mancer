3fc4b6918d7e1587a72d34c678bb15a2b46cd98d
Cristian Beltran <cbeltranc6@gmail.com>
2019-12-20 14:32:46 -0300

    Actualizacion servidor

--- models/ficha.model.js
+++ models/ficha.model.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 //Datos Fijos 
 
-const arquetipoSchema = new Schema({
+const campoSchema = new Schema({
     nombre: {type:String},
     valor: {type:String},
     descripcion: {type:String}
@@ -13,14 +13,15 @@ const arquetiposSchema = new Schema({
     usuario:        {type: mongoose.Schema.Types.ObjectId, ref: 'usuario'},
     fecha_atencion: {type:Date},
     nombre:         {type:String},
-    campos:         [arquetipoSchema]
+    campos:         [campoSchema],
+    editable:       {type:Boolean, default: true}
 })
 
 const fichaSchema = new Schema({
     folio: { type: Number, required: [true, "Numero de ingrso del paciente"] },
     paciente: { type: mongoose.Schema.Types.ObjectId, ref: 'Paciente'},
     fecha_ingreso: { type: Date, requiered: [true, "Primer Ingreso del Paciente "] },
-    arquetipos: [ arquetiposSchema]
-
+    arquetipos: [ arquetiposSchema],
+    last_update: {type: Date}
 });
 module.exports = mongoose.model('ficha', fichaSchema);
--- routes/api.routes.js
+++ routes/api.routes.js
@@ -4,7 +4,8 @@ const usuarioRoutes = require('./usuario.routes');
 const loginRoutes = require('./login.routes');
 const ficharoutes = require('./ficha.routes');
 const arquetiposRoutes = require('./arquetipos.routes');
-
+const noticiaRoutes=require('./noticias.routes');
+const uploadsRoutes = require('./upload.routes');
 
 let app = express();
 
@@ -12,5 +13,8 @@ app.use('/usuario', usuarioRoutes);
 app.use('/login', loginRoutes);
 app.use('/ficha', ficharoutes);
 app.use('/arquetipos', arquetiposRoutes);
+app.use('/noticias',noticiaRoutes);
+app.use('/noticias',noticiaRoutes);
+app.use('/uploads', uploadsRoutes);
 
 module.exports = app;
